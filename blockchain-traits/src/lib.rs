fa63eb692f289506f6331e656be331b022adfc1f
Nick Hynes <nhynes@oasislabs.com>
2019-05-18 18:29:36 +0000

    Rename blockchain intrinsics functions

--- blockchain-traits/src/lib.rs
+++ blockchain-traits/src/lib.rs
@@ -16,14 +16,22 @@ pub trait KVStore {
 
 pub trait BlockchainIntrinsics {
     /// Returns the input provided by the calling context.
-    fn input(&self) -> Vec<u8>;
+    fn fetch_input(&self) -> Vec<u8>;
     fn input_len(&self) -> u64;
 
     /// Returns data to the calling context.
     fn ret(&mut self, data: Vec<u8>);
 
     /// Returns error data to the calling context.
-    fn ret_err(&mut self, data: Vec<u8>);
+    fn err(&mut self, data: Vec<u8>);
+
+    /// Returns the `ret` data of the called transaction.
+    fn fetch_ret(&self) -> Vec<u8>;
+    fn ret_len(&self) -> u64;
+
+    /// Returns the `err` data of the called transaction.
+    fn fetch_err(&self) -> Vec<u8>;
+    fn err_len(&self) -> u64;
 
     /// Requests that an event be emitted in this block.
     fn emit(&mut self, topics: Vec<[u8; 32]>, data: Vec<u8>);

38ce989da71c93e941e724d295d15bb324193550
Nick Hynes <nhynes@oasislabs.com>
2019-06-29 12:50:26 +0000

    Add importer to mantle-rpc

---
+++ mantle-rpc/src/importer/mod.rs
@@ -0,0 +1,83 @@
+mod file;
+
+use crate::Interface;
+
+pub struct Importer {
+    backend: Box<dyn ImporterBackend>,
+}
+
+impl Importer {
+    pub fn for_url(url_str: &str) -> Result<Self, ImporterError> {
+        let url = match url::Url::parse(&url_str) {
+            Ok(url) => url,
+            Err(err) => return Err(ImporterError::InvalidUrl(err)),
+        };
+        Ok(Self {
+            backend: match url.scheme() {
+                "file" => {
+                    let url_path_start = url_str.find(url.path()).unwrap();
+                    let path_start = url_str
+                        .match_indices('/')
+                        .nth(2)
+                        .map(|ind| ind.0)
+                        .unwrap_or(url_path_start);
+                    let path = if path_start == url_path_start {
+                        // absolute path
+                        url.to_file_path().map_err(|_| {
+                            ImporterError::Fail(failure::format_err!(
+                                "Could not determine file path for URL: `{}`",
+                                url_str
+                            ))
+                        })?
+                    } else {
+                        let relpath_str =
+                            &url_str[(path_start + 1)..(url_path_start + url.path().len())];
+                        std::path::Path::new(relpath_str)
+                            .canonicalize()
+                            .map_err(|err| {
+                                ImporterError::Fail(failure::format_err!(
+                                    "{}: {}",
+                                    err,
+                                    relpath_str
+                                ))
+                            })?
+                    };
+                    box file::FileImporter { path }
+                }
+                _ => return Err(ImporterError::NoImporter(url.scheme().to_string())),
+            },
+        })
+    }
+
+    pub fn import(&self, name: &str) -> Result<Interface, ImporterError> {
+        self.backend.import(name)
+    }
+
+    pub fn import_all(&self) -> Result<Vec<Interface>, ImporterError> {
+        self.backend.import_all()
+    }
+}
+
+trait ImporterBackend {
+    fn import(&self, name: &str) -> Result<Interface, ImporterError>;
+
+    fn import_all(&self) -> Result<Vec<Interface>, ImporterError>;
+}
+
+#[derive(Debug, failure::Fail)]
+pub enum ImporterError {
+    #[fail(display = "Invalid URL: {}", _0)]
+    InvalidUrl(#[fail(cause)] url::ParseError),
+
+    #[fail(display = "No importer for scheme `{}`", _0)]
+    NoImporter(String),
+
+    #[fail(display = "{}", _0)]
+    Fail(#[fail(cause)] failure::Error),
+
+    #[fail(display = "Wasm module missing mantle-interface section")]
+    MissingInterfaceSection,
+
+    #[fail(display = "Could not locate `{}`", _0)]
+    NoImport(String),
+}

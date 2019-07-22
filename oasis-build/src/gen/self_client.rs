86a62ab78e84256359f2a424101273428b9c2816
Nick Hynes <nhynes@oasislabs.com>
2019-07-22 16:09:44 +0000

    WIP

--- oasis-build/src/gen/self_client.rs
+++ oasis-build/src/gen/self_client.rs
@@ -27,10 +27,22 @@ pub fn insert(build_ctx: &BuildContext, krate: &mut Crate, service_def: &Service
         ctor,
     } = service_def;
 
-    let client = if build_ctx.target == BuildTarget::Wasi {
+    let client_impl = if build_ctx.target == BuildTarget::Wasi {
         build_wasi_client(service_def)
     } else {
         build_test_client(service_def)
+    };
+
+    let client_ident = format_ident!("{}Client", sanitize_ident(&interface.name).to_camel_case());
+
+    let client = quote! {
+        pub struct #client_ident {
+            pub address: oasis_std::Address,
+        }
+
+        impl #client_ident {
+            #client_impl
+        }
     };
 
     let include_file = out_dir.join(format!("{}_client.rs", crate_name));

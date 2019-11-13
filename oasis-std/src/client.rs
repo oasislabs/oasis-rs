d7f5a58a1dc3e997a1514b16e398b95907b3d70c
Nick Hynes <nhynes@oasislabs.com>
2019-11-13 22:45:23 +0000

    Use forked borsh

--- oasis-std/src/client.rs
+++ oasis-std/src/client.rs
@@ -1,13 +1,11 @@
 use oasis_types::Address;
 
 pub trait Gateway {
-    type Error;
-
     /// Deploys a new service with the provided initcode.
     /// `initcode` is expected to be the Wasm bytecode concatenated with the the constructor stdin.
     /// Upon success, returns the address of the new service.
-    fn deploy(&self, initcode: &[u8]) -> Result<Address, Self::Error>;
+    fn deploy(&self, initcode: &[u8]) -> Result<Address, Box<dyn std::error::Error>>;
 
     /// Returns the output of calling the service at `address` with `data` as stdin.
-    fn rpc(&self, address: Address, data: &[u8]) -> Result<Vec<u8>, Self::Error>;
+    fn rpc(&self, address: Address, data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>>;
 }

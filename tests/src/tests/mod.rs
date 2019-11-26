32d9ed91625051478e638b59d8c9eb6825a0d6d5
Nick Hynes <nhynes@oasislabs.com>
2019-11-26 18:03:00 +0000

    Use wasmparser for reading oasis-interface

--- tests/src/tests/mod.rs
+++ tests/src/tests/mod.rs
@@ -5,14 +5,8 @@ pub fn test_oasis_interface(bin_name: &str, service_name: &str) {
     let mf_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     let wasm_path = mf_dir.join(format!("../target/wasm32-wasi/release/{}.wasm", bin_name));
 
-    let iface_bytes = walrus::Module::from_file(&wasm_path)
-        .unwrap_or_else(|_| panic!("No wasm at `{}`", wasm_path.display()))
-        .customs
-        .remove_raw("oasis-interface")
-        .expect("No custom")
-        .data;
-
-    let actual = oasis_rpc::Interface::from_slice(&iface_bytes).unwrap();
+    let actual =
+        oasis_rpc::Interface::from_wasm_bytecode(&std::fs::read(&wasm_path).unwrap()).unwrap();
 
     let json_path = mf_dir.join(format!("res/{}.json", service_name));
     let expected: oasis_rpc::Interface =
